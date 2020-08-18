e5e690692c862062dc0445c7cbb23f1841b6356d
Elias Khan <khan.elias@gmail.com>
2020-08-18 22:41:43 +0200

    display messages in chat-window div

--- public/script.js
+++ public/script.js
@@ -1,6 +1,13 @@
 const socket = io()
 const chat = document.querySelector('.chat-form')
 const Input = document.querySelector('.chat-input')
+const chatWindow = document.querySelector('.chat-window')
+const renderMessage = message => {
+    const div = document.createElement('div')
+    div.classList.add('render-message')
+    div.innerText = message
+    chatWindow.appendChild(div)
+}
 
 chat.addEventListener('submit', event => {
     event.preventDefault()
@@ -9,5 +16,6 @@ chat.addEventListener('submit', event => {
 })
 
 socket.on('chat', message => {
-    console.log('From server: ', message)
+    //console.log('From server: ', message)
+    renderMessage(message)
 })
