d3d1da34c896d65bf0842627856071151ca6415f
Elias Khan <khan.elias@gmail.com>
2020-08-18 21:41:30 +0200

    lay the groundwork for socket in index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
-
 const app = express()
 const server = require('http').createServer(app)
 const port = process.env.PORT || 3000
+const path = require('path')
+const io = require('socket.io')(server)
 
-// just to test the server
-app.get('/', (req, res) => {
-  res.status(200).send('Working')
+app.use(express.static(path.join(__dirname + '/public')))
+
+io.on('connection', socket => {
+    console.log('Some client connected')
 })
 
 server.listen(port, () => {
-  console.log(`Server running on port: ${port}`)
+    console.log(`Server running on port: ${port}`)
 })
